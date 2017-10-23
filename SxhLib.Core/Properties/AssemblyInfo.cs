fa27006a58f96c1c04b54b8f349c6123c63fa9b6
shixiaohui127 <shixiaohui127@163.com>
2017-10-23 14:23:00 +0800

    init

--- SxhLib.Core/Properties/AssemblyInfo.cs
+++ SxhLib.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("CSharp-Infrastructure-Core")]
+[assembly: AssemblyTitle("SxhLib.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSharp-Infrastructure-Core")]
+[assembly: AssemblyProduct("SxhLib.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("50dbc585-2e40-4019-acde-9a36c932cf0d")]
+[assembly: Guid("db9996a9-57ef-4dec-a8bb-30e0649871c2")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
